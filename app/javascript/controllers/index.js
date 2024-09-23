74b5f82a457cca881811172c70bdb50f29d1e454
Ryuta0210 <ryutoyoda0210@gmail.com>
2024-09-23 14:17:35 +0900

    PriceのJS実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,18 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+function calculation(){
+  const showTheCost = document.getElementById('item-price').addEventListener('input', function(){
+    const price = parseInt(this.value)
+    const feePercentage = 0.1
+    const fee = price * feePercentage
+    const profit = price - fee
+
+    document.getElementById('add-tax-price').textContent = Math.floor(fee)
+    document.getElementById('profit').textContent = Math.floor(profit)
+  })
+}
+
+
+window.addEventListener("load", calculation)
