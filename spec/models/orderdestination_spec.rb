dc1b691bc71d595515ed9e6f01046ff4bb603160
Ryuta0210 <ryutoyoda0210@gmail.com>
2024-10-03 15:54:17 +0900

    テストコードの修正

--- spec/models/orderdestination_spec.rb
+++ spec/models/orderdestination_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 RSpec.describe OrderDestination, type: :model do
   before do
     user = FactoryBot.create(:user)
-    @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: 1)
+    item = FactoryBot.create(:item)
+    @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: item.id)
   end
 
   describe '購入登録' do
