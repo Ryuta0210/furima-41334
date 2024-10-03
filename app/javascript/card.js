58dce226d1808eb76842338b4188e54e5594e292
Ryuta0210 <ryutoyoda0210@gmail.com>
2024-10-03 17:36:22 +0900

    console追記

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
   const publicKey = gon.public_key
+  console.log("OK")
   const payjp = Payjp(publicKey)
   const elements = payjp.elements()
   const numberElement = elements.create("cardNumber")
